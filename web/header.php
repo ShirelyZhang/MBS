fed2312798ff0fc179bf60778cba383048a05c3b
ShirelyZhang <changmeiz@163.com>
2014-11-04 17:50:55 +0800

    use bootstrap to refine header and footer style

--- web/header.php
+++ web/header.php
@@ -1,6 +1,6 @@
 <div class="header-block">
-  <h2 class="logo-title"><a href="index.php" title="Message Board">Message Board</a></h2>
-  <div class="nav-bar">
+  <h2 class="pull-left"><a href="index.php" class="logo-link" title="Message Board">Message Board</a></h2>
+  <div class="nav-bar pull-left">
     <a href="index.php" class="nav-link" data-type="0">All Messages</a>
     <a href="my-messages.php" class="nav-link" data-type="1">My Messages</a>
     <!--<a href="my-profile.php" class="nav-link" data-type="2">My Profile</a>-->
@@ -9,14 +9,14 @@
   	session_start();
   	if (isset($_SESSION['nickname'])) {
   ?>
-	<div class="welcome-block">
+	<div class="welcome-block pull-right">
 		<span class="welcome-info"><?php echo htmlspecialchars($_SESSION['nickname']); ?></span>
 		<a href="logout.php">Logout</a>
 	</div>
   <?php
   	} else {
   ?>
-  	<div class="log-btns">
+  	<div class="log-btns pull-right">
 	    <a href="login.php">Login</a>
 	    <span>|</span>
 	    <a href="register.php">Register</a>
@@ -24,5 +24,5 @@
   <?php
   	}
   ?>
+  <div class="clearfix"></div>
 </div>
-<!--<script type="text/javascript" src="js/header.js"></script>-->
--- web/my-messages.php
+++ web/my-messages.php
@@ -9,49 +9,54 @@ session_start();
   	<meta name="Content-Type" content="text/html; charset=utf-8">
     <title>Message Board</title>
     <link rel="stylesheet" type="text/css" href="libs/cssreset-min.css">
+    <link rel="stylesheet" type="text/css" href="libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="libs/bootstrap/css/bootstrap-theme.min.css">
     <link rel="stylesheet" type="text/css" href="css/common.css">
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" type="text/css" href="css/my-messages.css">
     <script type="text/javascript" src="libs/jquery/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="libs/bootstrap/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/my-messages.js"></script>
   </head>
   <body>
-    <header class="header">
-        <?php include_once('header.php'); ?>
-      </header>
-    <div class="page">
-      <div class="content">
-        <div class="filter-section">
-          <form id="messageSearchForm">
-            <input type="hidden" name="message-author" value="<?php echo $_SESSION['nickname'];?>">
-            <input type="text" name="message-title" placeholder="Title">
-            <input type="text" name="message-content" placeholder="Content">
-            <input type="button" value="Search" id="searchBtn">
-            <a href="create.php" id="createLink">Create Message</a>
-          </form>
-        </div>
-        <div class="message-list-block">
-          <div class="pagination-layer"><?php include 'pagination.php'; ?></div>
-          <div class="message-list-table">
-            <table id="messageListTable">
-              <thead>
-                <th class="col-id">ID</th>
-                <th class="col-title">Title</th>
-                <th class="col-author">Author</th>
-                <th class="col-created">Created</th>
-                <th class="col-operation">Operation</th>
-              </thead>
-              <tbody id="messageListTbody">
-                <tr><td colspan="5">No Record</td></tr>
-              </tbody>
-            </table>
+    <div class="container">
+      <header class="row">
+          <?php include_once('header.php'); ?>
+        </header>
+      <div class="row page">
+        <div class="content">
+          <div class="filter-section">
+            <form id="messageSearchForm">
+              <input type="hidden" name="message-author" value="<?php echo $_SESSION['nickname'];?>">
+              <input type="text" name="message-title" placeholder="Title">
+              <input type="text" name="message-content" placeholder="Content">
+              <input type="button" value="Search" id="searchBtn">
+              <a href="create.php" id="createLink">Create Message</a>
+            </form>
           </div>
-          <div class="pagination-layer"><?php include 'pagination.php'; ?></div>
+          <div class="message-list-block">
+            <div class="pagination-layer"><?php include 'pagination.php'; ?></div>
+            <div class="message-list-table">
+              <table id="messageListTable">
+                <thead>
+                  <th class="col-id">ID</th>
+                  <th class="col-title">Title</th>
+                  <th class="col-author">Author</th>
+                  <th class="col-created">Created</th>
+                  <th class="col-operation">Operation</th>
+                </thead>
+                <tbody id="messageListTbody">
+                  <tr><td colspan="5">No Record</td></tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="pagination-layer"><?php include 'pagination.php'; ?></div>
+          </div>
         </div>
+        <footer class="row">
+          <?php include_once('footer.php'); ?>
+        </footer>
       </div>
-      <footer class="footer">
-        <?php include_once('footer.php'); ?>
-      </footer>
     </div>
   </body>
 </html>
